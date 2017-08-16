5a5c9aa895d471147f418646b73e144a94254f1d
Nicolas <nicolasgodoyrojas20@gmail.com>
2017-08-16 13:18:29 -0300

    Hicimos los test de algunos metodos

--- src/test/TestNumero.java
+++ src/test/TestNumero.java
@@ -13,11 +13,37 @@ public class TestNumero {
 		Numero numero1 = new Numero(7);
 		Numero numero2 = new Numero(1857);
 		Numero numero3 = new Numero (4);
+		System.out.println("Primo: Verdadero o falso");
+		System.out.println("¿Es numero primo el numero "+numero1.getN()+"? "+numero1.esPrimo());
+		System.out.println("¿Es numero primo el numero "+numero2.getN()+"? "+numero2.esPrimo());
+		System.out.println("¿Es numero primo el numero "+numero3.getN()+"? "+numero3.esPrimo());
 		
-		System.out.println(numero1.getN());
-		System.out.println("¿Es numero primo? "+numero1.esPrimo());
-		System.out.println("¿Es numero primo? "+numero2.esPrimo());
-		System.out.println("¿Es numero primo? "+numero3.esPrimo());
+		System.out.println("Sumas:");
+		System.out.println(numero1.getN()+"+5 ="+numero1.sumar(5));
+		System.out.println(numero2.getN()+"+20 ="+numero2.sumar(20));
+		System.out.println(numero3.getN()+"+6 ="+numero3.sumar(6));
+		
+		System.out.println("Multiplicaciones");
+		System.out.println(numero1.getN()+"*5 ="+numero1.multiplicar(5));
+		System.out.println(numero2.getN()+"*20 ="+numero2.multiplicar(20));
+		System.out.println(numero3.getN()+"*6 ="+numero3.multiplicar(6));
+		
+		System.out.println("Es par: Verdadero o falso");
+		System.out.println(numero1.getN()+" ? "+numero1.esPar());
+		System.out.println(numero2.getN()+" ? "+numero2.esPar());
+		System.out.println(numero3.getN()+" ? "+numero3.esPar());
+		
+		
+		System.out.println("Calcular potencia");
+		
+		System.out.println(numero1.getN()+" = "+numero1.calcularPotencia(2));
+		System.out.println(numero2.getN()+" = "+numero2.calcularPotencia(2));
+		System.out.println(numero3.getN()+" = "+numero3.calcularPotencia(2));
+		
+		
+				
+		
+		
 	}
 
 }
