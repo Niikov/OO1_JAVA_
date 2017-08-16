99c816d1adb7ada304b39c64aa1f7e58f2ff0453
Nicolas <nicolasgodoyrojas20@gmail.com>
2017-08-16 10:50:33 -0300

    Agregamos metodos y arregamos metodo esPrimo

--- src/modelo/Numero.java
+++ src/modelo/Numero.java
@@ -37,15 +37,21 @@ public class Numero {
 	
 	public boolean esPrimo(){
 		int contador;
-		boolean resultado = false;
-		for( contador= this.n-1 ; contador >= 1; --contador){
-			if ( contador%2 == 0 ){
-				resultado = false;}
-			else {resultado = true;}
+		int resultado=0;
+		
+		for (contador = 1; contador<=this.n; contador++){
+			if ( this.n%contador == 0){
+				++resultado;
+			}
 			
 		}
 		
-		return resultado;
+		if (resultado ==2){
+			return true;
+			
+		}else {return false;}
+		
+	
 		
 	}
 	
@@ -53,9 +59,69 @@ public class Numero {
 		return String.valueOf(n);
 		
 	}
+	
+	public double convertirDouble(){
+		double resultado;
+		resultado = Double.parseDouble(this.convertirAString());
+		return resultado;
 		
+	}
+	
+	public double calcularPotencia (int exp){
+		
+		return Math.pow(this.convertirDouble(), Double.parseDouble(String.valueOf(exp)));
+	}
+	
+	public String pasarBase2(){
+		int resultado ;
+		resultado = (this.n* this.n);
+		return String.valueOf(resultado);
+		
+	}
+	
+	public int calcularFactorial(){
+		int resultado=0;
+		int contador;
+		if (this.n <= 0 ){
+			resultado = 1;
+			return resultado;
+		} else {
+			for ( contador = 1; contador < this.n ; contador--){
+				resultado = resultado + ( contador * (contador+1));}
+			
+			return resultado;}
+		
+	}
+		
+		public int calcularFactorial(int n1){
+			int resultado=0;
+			int contador;
+			if (n1 <= 0 ){
+				resultado = 1;
+				return resultado;
+			} else {
+				for ( contador = 1; contador < n1 ; contador--){
+					resultado = resultado + ( contador * (contador+1));}
+				
+				return resultado;
+			}
+		
+	}
+	
+	public double numeroCombinatorio(int n1){
+		double resultado=0;
+		if(  this.n >= 0 && n1 >=0){
+			
+			resultado = this.calcularFactorial()/ this.calcularFactorial(n1)*this.calcularFactorial(this.n-n1);
+			return resultado;
+		} else {return resultado;}
+	}	
 		
 }
+
+
+		
+		
 
 
 	
